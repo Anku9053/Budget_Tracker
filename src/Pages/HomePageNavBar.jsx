506d5e024e0a6384838cf23fe89bb7a18a3f8827
Ankesh Kumar <ankesh9053@gmail.com>
2024-01-30 15:11:27 +0530

    done with the basic navbar

--- src/Pages/HomePageNavBar.jsx
+++ src/Pages/HomePageNavBar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+// Navbar.js
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background-color: #333;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  color: #fff;
+  margin: 0;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+`;
+
+const NavLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 20px;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const HomePageNavBar = () => {
   return (
-    <div>HomePageNavBar</div>
-  )
-}
+    <Nav>
+      <Logo>Budget Tracker</Logo>
+      <NavLinks>
+        <NavLink href="#">Home</NavLink>
+        <NavLink href="#">Transactions</NavLink>
+        <NavLink href="#">Categories</NavLink>
+        
+      </NavLinks>
+    </Nav>
+  );
+};
 
-export default HomePageNavBar
+export default HomePageNavBar;
